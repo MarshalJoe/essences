ac69cd4126306ceffbda126985b8dbc78fd1b436
Joe M <jmarshall@codero.com>
2015-09-07 15:05:01 -0500

    Upgrades header and splash image

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,15 +8,17 @@
  * Controller of the storefrontApp
  */
 angular.module('storefrontApp')
-.controller('MainCtrl', function($scope, MoltinAuth) {
-
-	$scope.awesomeThings = ['hel', 'something', 'hey'];
+.controller('MainCtrl', function($scope, $location, MoltinAuth) {
 
 	MoltinAuth.then(function(moltin) {
         moltin.Category.List(null, function(categories) {
         	$scope.categories = categories;
         	console.log(categories);
         });
-    })
+    });
+
+    $scope.go = function (path) {
+    	$location.path(path);
+    }
 
 });
